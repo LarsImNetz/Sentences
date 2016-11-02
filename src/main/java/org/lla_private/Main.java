117bb38c8c6d22c756d34b359a915c430fe33aff
Lars Langhans <lars.langhans@gmx.de>
2016-11-02 23:17:01 +0100

    Einfaches Main hinzugefügt

---
+++ src/main/java/org/lla_private/Main.java
@@ -0,0 +1,10 @@
+package org.lla_private;
+
+import text.SatzDreher;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println(new SatzDreher("Hello World.").getSatz());
+	}
+}
