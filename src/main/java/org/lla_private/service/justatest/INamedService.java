4d931a949320708ae5731f79571260426b514294
Lars Langhans <lars.langhans@hypoport.de>
2017-01-19 11:45:06 +0100

    Alles entfernt, was mit Service zu tun hat

--- src/main/java/org/lla_private/service/justatest/INamedService.java
+++
@@ -1,5 +0,0 @@
-package org.lla_private.service.justatest;
-
-public interface INamedService {
-	String getHello();
-}
--- src/main/java/org/lla_private/service/satzdreher/ISatzDreherService.java
+++
@@ -1,5 +0,0 @@
-package org.lla_private.service.satzdreher;
-
-public interface ISatzDreherService {
-	String satzDrehen(String satz);
-}
