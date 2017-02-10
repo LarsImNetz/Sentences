c29b4e3c32edd65bdcadbf35598ac3b3d6004577
Lars Langhans <lars.langhans@hypoport.de>
2017-02-10 09:58:16 +0100

    Fraktur und Schreibschrift hinzugefügt

---
+++ src/main/java/text/manipulation/word/BuchstabenToFraktur.java
@@ -0,0 +1,206 @@
+package text.manipulation.word;
+
+import com.google.common.base.Preconditions;
+
+public class BuchstabenToFraktur implements IWord {
+
+	private String word;
+
+	public BuchstabenToFraktur() {
+	}
+
+	@Override
+	public void setWord(String word) {
+		Preconditions.checkArgument(word != null);
+		this.word = word;
+	}
+
+	@Override
+	public String getWord() {
+		return perform();
+	}
+
+	private String perform() {
+		StringBuilder buffer = new StringBuilder();
+		for (char buchstabe : word.toCharArray()) {
+			String charAsString = String.valueOf(buchstabe);
+			buffer.append(convertOnlyUppercase(convertOnlyLowercase(charAsString)));
+		}
+		return buffer.toString();
+	}
+
+	private String convertOnlyUppercase(String character) {
+		String neu = "";
+		switch (character) {
+			case "A":
+				neu = "&Afr;"; // Aoopf
+				break;
+			case "B":
+				neu = "&Bfr;";
+				break;
+			case "C":
+				neu = "&Cfr;";
+				break;
+			case "D":
+				neu = "&Dfr;";
+				break;
+			case "E":
+				neu = "&Efr;";
+				break;
+			case "F":
+				neu = "&Ffr;";
+				break;
+			case "G":
+				neu = "&Gfr;";
+				break;
+			case "H":
+				neu = "&Hfr;";
+				break;
+			case "I":
+				neu = "&Ifr;";
+				break;
+			case "J":
+				neu = "&Jfr;";
+				break;
+			case "K":
+				neu = "&Kfr;";
+				break;
+			case "L":
+				neu = "&Lfr;";
+				break;
+			case "M":
+				neu = "&Mfr;";
+				break;
+			case "N":
+				neu = "&Nfr;";
+				break;
+			case "O":
+				neu = "&Ofr;";
+				break;
+			case "P":
+				neu = "&Pfr;";
+				break;
+			case "Q":
+				neu = "&Qfr;";
+				break;
+			case "R":
+				neu = "&Rfr;";
+				break;
+			case "S":
+				neu = "&Sfr;";
+				break;
+			case "T":
+				neu = "&Tfr;";
+				break;
+			case "U":
+				neu = "&Ufr;";
+				break;
+			case "V":
+				neu = "&Vfr;";
+				break;
+			case "W":
+				neu = "&Wfr;";
+				break;
+			case "X":
+				neu = "&Xfr;";
+				break;
+			case "Y":
+				neu = "&Yfr;";
+				break;
+			case "Z":
+				neu = "&Zfr;";
+				break;
+			default:
+				neu = character;
+		}
+		return neu;
+	}
+
+	private String convertOnlyLowercase(String character) {
+		String neu = "";
+		switch (character) {
+			case "a":
+				neu = "&afr;"; // Aoopf
+				break;
+			case "b":
+				neu = "&bfr;";
+				break;
+			case "c":
+				neu = "&cfr;";
+				break;
+			case "d":
+				neu = "&dfr;";
+				break;
+			case "e":
+				neu = "&efr;";
+				break;
+			case "f":
+				neu = "&ffr;";
+				break;
+			case "g":
+				neu = "&gfr;";
+				break;
+			case "h":
+				neu = "&hfr;";
+				break;
+			case "i":
+				neu = "&ifr;";
+				break;
+			case "j":
+				neu = "&jfr;";
+				break;
+			case "k":
+				neu = "&kfr;";
+				break;
+			case "l":
+				neu = "&lfr;";
+				break;
+			case "m":
+				neu = "&mfr;";
+				break;
+			case "n":
+				neu = "&nfr;";
+				break;
+			case "o":
+				neu = "&ofr;";
+				break;
+			case "p":
+				neu = "&pfr;";
+				break;
+			case "q":
+				neu = "&qfr;";
+				break;
+			case "r":
+				neu = "&rfr;";
+				break;
+			case "s":
+				neu = "&sfr;";
+				break;
+			case "t":
+				neu = "&tfr;";
+				break;
+			case "u":
+				neu = "&ufr;";
+				break;
+			case "v":
+				neu = "&vfr;";
+				break;
+			case "w":
+				neu = "&wfr;";
+				break;
+			case "x":
+				neu = "&xfr;";
+				break;
+			case "y":
+				neu = "&yfr;";
+				break;
+			case "z":
+				neu = "&zfr;";
+				break;
+			default:
+				neu = character;
+		}
+		return neu;
+	}
+
+}
---
+++ src/main/java/text/manipulation/word/BuchstabenToSchreibschrift.java
@@ -0,0 +1,206 @@
+package text.manipulation.word;
+
+import com.google.common.base.Preconditions;
+
+public class BuchstabenToSchreibschrift implements IWord {
+
+	private String word;
+
+	public BuchstabenToSchreibschrift() {
+	}
+
+	@Override
+	public void setWord(String word) {
+		Preconditions.checkArgument(word != null);
+		this.word = word;
+	}
+
+	@Override
+	public String getWord() {
+		return perform();
+	}
+
+	private String perform() {
+		StringBuilder buffer = new StringBuilder();
+		for (char buchstabe : word.toCharArray()) {
+			String charAsString = String.valueOf(buchstabe);
+			buffer.append(convertOnlyUppercase(convertOnlyLowercase(charAsString)));
+		}
+		return buffer.toString();
+	}
+
+	private String convertOnlyUppercase(String character) {
+		String neu = "";
+		switch (character) {
+			case "A":
+				neu = "&Ascr;"; // Aoopf
+				break;
+			case "B":
+				neu = "&Bscr;";
+				break;
+			case "C":
+				neu = "&Cscr;";
+				break;
+			case "D":
+				neu = "&Dscr;";
+				break;
+			case "E":
+				neu = "&Escr;";
+				break;
+			case "F":
+				neu = "&Fscr;";
+				break;
+			case "G":
+				neu = "&Gscr;";
+				break;
+			case "H":
+				neu = "&Hscr;";
+				break;
+			case "I":
+				neu = "&Iscr;";
+				break;
+			case "J":
+				neu = "&Jscr;";
+				break;
+			case "K":
+				neu = "&Kscr;";
+				break;
+			case "L":
+				neu = "&Lscr;";
+				break;
+			case "M":
+				neu = "&Mscr;";
+				break;
+			case "N":
+				neu = "&Nscr;";
+				break;
+			case "O":
+				neu = "&Oscr;";
+				break;
+			case "P":
+				neu = "&Pscr;";
+				break;
+			case "Q":
+				neu = "&Qscr;";
+				break;
+			case "R":
+				neu = "&Rscr;";
+				break;
+			case "S":
+				neu = "&Sscr;";
+				break;
+			case "T":
+				neu = "&Tscr;";
+				break;
+			case "U":
+				neu = "&Uscr;";
+				break;
+			case "V":
+				neu = "&Vscr;";
+				break;
+			case "W":
+				neu = "&Wscr;";
+				break;
+			case "X":
+				neu = "&Xscr;";
+				break;
+			case "Y":
+				neu = "&Yscr;";
+				break;
+			case "Z":
+				neu = "&Zscr;";
+				break;
+			default:
+				neu = character;
+		}
+		return neu;
+	}
+
+	private String convertOnlyLowercase(String character) {
+		String neu = "";
+		switch (character) {
+			case "a":
+				neu = "&ascr;"; // Aoopf
+				break;
+			case "b":
+				neu = "&bscr;";
+				break;
+			case "c":
+				neu = "&cscr;";
+				break;
+			case "d":
+				neu = "&dscr;";
+				break;
+			case "e":
+				neu = "&escr;";
+				break;
+			case "f":
+				neu = "&fscr;";
+				break;
+			case "g":
+				neu = "&gscr;";
+				break;
+			case "h":
+				neu = "&hscr;";
+				break;
+			case "i":
+				neu = "&iscr;";
+				break;
+			case "j":
+				neu = "&jscr;";
+				break;
+			case "k":
+				neu = "&kscr;";
+				break;
+			case "l":
+				neu = "&lscr;";
+				break;
+			case "m":
+				neu = "&mscr;";
+				break;
+			case "n":
+				neu = "&nscr;";
+				break;
+			case "o":
+				neu = "&oscr;";
+				break;
+			case "p":
+				neu = "&pscr;";
+				break;
+			case "q":
+				neu = "&qscr;";
+				break;
+			case "r":
+				neu = "&rscr;";
+				break;
+			case "s":
+				neu = "&sscr;";
+				break;
+			case "t":
+				neu = "&tscr;";
+				break;
+			case "u":
+				neu = "&uscr;";
+				break;
+			case "v":
+				neu = "&vscr;";
+				break;
+			case "w":
+				neu = "&wscr;";
+				break;
+			case "x":
+				neu = "&xscr;";
+				break;
+			case "y":
+				neu = "&yscr;";
+				break;
+			case "z":
+				neu = "&zscr;";
+				break;
+			default:
+				neu = character;
+		}
+		return neu;
+	}
+
+}
